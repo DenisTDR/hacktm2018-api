8200ca64f2087ef44336c4f459043782c0ba24bb
Erik Blenert <eblenert@atrisktech.com>
2018-05-13 14:05:55 +0300

    Fixed sputnik thumbnail.

--- src/services/article-crawler.service.ts
+++ src/services/article-crawler.service.ts
@@ -79,7 +79,11 @@ export default class ArticleCrawler {
         const title = $('title').first().text();
         const articleBody = $('.b-article__text p').text();
         const articleLead = $('.b-article__text p').text();
-        const thumbnail = $('.b-inject__media img').attr('src');
+        let thumbnail = $('.b-inject__media img').attr('src');
+
+        if (typeof thumbnail === 'undefined') {
+            thumbnail = $('.b-article__header img').attr('src');
+        }
 
         let publication = await Publication.findOne({ alias: this.site }).exec();
 
